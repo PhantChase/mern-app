a66fedcc2cd23bd2af39bb77199dd3614096c33a
PhantChase <ptdung.18it3@vku.udn.vn>
2021-07-29 20:40:30 +0700

    update post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
+const verifyToken = require('../midddleware/auth')
+
+// @route GET api/posts
+// @sedc Get posts
+// @access Private 
+router.get('/', verifyToken, async(req, res) =>{
+    try {
+        const posts = await Post.find({user: req.userId}).populate('user', ['username'])
+        res.json({success: true, posts})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, message: 'server error'})
+    }
+})
+
 
 // @route POST api/posts
 // @sedc Create post
 // @access Private 
-router.post('/', async(req, res) => {
+router.post('/', verifyToken, async(req, res) => {
     const{title, description, url, status} = req.body
     //Simple calidation
     if (!title)
@@ -19,12 +34,45 @@ router.post('/', async(req, res) => {
             description,
             url: url.startsWith('https://') ? url :`https://${url}`,
             status: status || 'TO LEARN',
-            user: '61029af0ced2a40aec105e3b'
+            user: req.userId
         })
 
         await newPost.save()
 
         res.json({success: true, message: 'Happy Learning!', post: newPost })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, message: 'server error'})
+    }
+})
+// @route PUT api/posts
+// @sedc Update post
+// @access Private 
+router.put('/:id', verifyToken, async(req, res) => {
+    const{title, description, url, status} = req.body
+    //Simple calidation
+    if (!title)
+        return res
+        .status(400)
+        .json({success: false, message: 'Title is requied'})
+
+    try {
+        let updatedPost = {
+            title,
+            description: description || '',
+            url : (url.startsWith('https://') ? url :`https://${url}`) || '',
+            status: status || 'TO LEARN'
+        }
+
+        const postUpdateCondition = {_id: req.params.id, user: req.userId}
+
+        updatedPost = await Post.findOneAndUpdate(postUpdateCondition, updatedPost, {new: true})
+
+        //User not auth to update post
+        if(!updatedPost)
+        return res.status(401).json({success: false, message: 'Post not found'})
+
+        res.json({success: true, message: 'Excellent progress', post: updatedPost})
     } catch (error) {
         console.log(error)
         res.status(500).json({success: false, message: 'server error'})
